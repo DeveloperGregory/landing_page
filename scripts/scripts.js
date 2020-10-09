70b39f1da00f7df75ab0a64dd63769b91032b9c2
Greg Seiler <ashimar229@gmail.com>
2020-10-09 12:58:22 -0400

    Worked on script added click to thumbs but not working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+
+
 const PICS = ["./images/drone.jpg","./images/drones-unmanned-aircraft.jpg","./images/images_medium.png"];
 const LEFTARROW = document.getElementById("left-arrow");
 const RIGHTARROW = document.getElementById("right-arrow");
@@ -7,18 +9,23 @@ const PRICE = document.getElementById('price');
 let currPic = 0;
 
 
-function changePic(change){
-    currPic += change;
-    currPic = (currPic > PICS.length-1) ? 0 : (currPic < 0) ? PICS.length-1 : currPic; 
+function changePic(change, isAdd){
+    if(isAdd){
+        currPic += change;
+        currPic = (currPic > PICS.length-1) ? 0 : (currPic < 0) ? PICS.length-1 : currPic; 
+    }else{
+        currPic = change;
+    }
     PICCONTAINER.innerHTML = `<img src="${PICS[currPic]}" alt="Drone Pic"> `;
 }
 
 function createThumbs(){
     for(var i = 0; i < PICS.length; i++){
-        THUMBCONTAINER.innerHTML += `<li><a href=''><img src="${PICS[i]}" alt="Drone Pic Thumbnails"></li>`;
+        THUMBCONTAINER.innerHTML += `<li id='${i}'><a href=''><img src="${PICS[i]}" alt="Drone Pic Thumbnails"></li>`;
+        
     }
-    
 }
+
 function displayPrice(currency){
     PRICE.innerHTML = `${currency}59.99`;
 }
@@ -26,7 +33,13 @@ function displayPrice(currency){
 
 changePic(0);
 createThumbs();
+const THUMB0 = document.getElementById(0);
+const THUMB1 = document.getElementById(1);
+const THUMB2 = document.getElementById(2);
 displayPrice('$');
-LEFTARROW.addEventListener("click", () => changePic(-1));
-RIGHTARROW.addEventListener("click", () => changePic(1));
+LEFTARROW.addEventListener("click", () => changePic(-1, true));
+RIGHTARROW.addEventListener("click", () => changePic(1, true));
+THUMB0.addEventListener('click', () => changePic(0,false));
+THUMB1.addEventListener('click', () => changePic(1,false));
+THUMB2.addEventListener('click', () => changePic(2,false));
 
